c21b669a4ccc3b67e52ee6f6bc3bbb0f962f49ce
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2022-01-01 15:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import base64
 import streamlit as st
 
 
-openai.api_key = "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" #openAI  API Key
+openai.api_key = "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" #openAI  API Key
 
 st.title("New Year Resolution Generator")
 def get_base64(bin_file):
